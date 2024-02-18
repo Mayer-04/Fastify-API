3a2a85f7070d5ce05047f5a4ebb1e15bd8355522
Mayer Andres <andres.20.chaves@gmail.com>
2024-02-18 01:11:03 -0500

    Cambios e implementaciones en las rutas

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,25 +1,51 @@
 import { FastifyInstance } from "fastify";
 import { pool } from "../database/postgres";
 import { Product } from "../types/products";
+import { QueryResult } from "pg";
 
 export const productRoutes = async (fastify: FastifyInstance) => {
-  fastify.get("/", async (request, reply) => {
-    const query = await pool.query("SELECT * FROM products");
-
-    reply.send(query);
+  fastify.get("/", async (_request, reply) => {
+    try {
+      const query: QueryResult<Product> = await pool.query(
+        "SELECT * FROM products"
+      );
+      const { rows } = query;
+      if (rows.length === 0) reply.code(404).send({ message: "No Products" });
+      return reply.send(rows);
+    } catch (error) {
+      reply.code(500).send({ message: error });
+    }
   });
 
-  fastify.get("/:id", async (request, reply) => {});
+  fastify.get<{ Params: { id: string } }>("/:id", async (request, reply) => {
+    const { id } = request.params;
+    try {
+      const query: QueryResult<Product> = await pool.query(
+        "SELECT name, description, price FROM products WHERE id = $1",
+        [id]
+      );
+      const { rows } = query;
+      if (rows.length === 0)
+        reply.code(404).send({ message: "Product not found" });
+      return reply.code(200).send(rows);
+    } catch (error) {
+      reply.code(500).send({ message: error });
+    }
+  });
 
   fastify.post<{ Body: Product }>("/", async (request, reply) => {
     const { name, description, price } = request.body;
 
-    const query = await pool.query(
-      "INSERT INTO products (name, description, price) VALUES ($1, $2, $3)",
-      [name, description, price]
-    );
+    try {
+      const query = await pool.query(
+        "INSERT INTO products (name, description, price) VALUES ($1, $2, $3)",
+        [name, description, price]
+      );
 
-    reply.code(201).send({ message: "Product created", product: query });
+      reply.code(201).send({ message: "Product created", product: query });
+    } catch (error) {
+      reply.code(500).send({ message: error });
+    }
   });
 
   fastify.put("/:id", async (request, reply) => {});
