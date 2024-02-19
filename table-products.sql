8989b194cca6103de3770e347ab2b6e26b1fc2f5
Mayer Andres <andres.20.chaves@gmail.com>
2024-02-19 16:07:21 -0500

    Añadiendo nueva columna a la tabla 🍫

--- table-products.sql
+++ table-products.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    description VARCHAR(150),
-    price INTEGER NOT NULL CHECK(price > 0)
+    description TEXT,
+    price INTEGER NOT NULL CHECK(price > 0),
+    quantity INTEGER NOT NULL
 );
